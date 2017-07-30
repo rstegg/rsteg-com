70135cd80072cfbddd18360cfb2e2da4a5e95f48
Ryan <stegartie@gmail.com>
2017-07-29 17:47:34 -0700

    Fix scrolling routing behavior

--- app/src/js/components/Dock.js
+++ app/src/js/components/Dock.js
@@ -1,26 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
+import { onScrolling, onScrollingEnd, updateScroll } from 'actions/ui'
 import { generate2xIcon } from 'utils/helpers'
 
-const Dock = ({ isScrolling }) =>
-  <div className={`dock ${isScrolling ? 'dock--hide' : ''}`}>
-    <a href='mailto:stegmannrd@gmail.com?subject=Hello' className='dock__item'>
-      <span className='dock__item-text'>Mail</span>
-      <i className={generate2xIcon('paper-plane')}></i>
-    </a>
-    <NavLink to='/' className='dock__item'>
-      <span className='dock__item-text'>Home</span>
-      <i className={generate2xIcon('home')}></i>
-    </NavLink>
-    <NavLink to='/blog' className='dock__item'>
-      <span className='dock__item-text'>Blog</span>
-      <i className={generate2xIcon('list')}></i>
-    </NavLink>
-    <NavLink to='/about/languages' className='dock__item'>
-      <span className='dock__item-text'>About</span>
-      <i className={generate2xIcon('portrait')}></i>
-    </NavLink>
-  </div>
+class Dock extends Component {
+  componentDidMount() {
+    window.addEventListener('scroll', () => this.scrollHandle())
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', () => this.scrollHandle())
+  }
+  scrollHandle() {
+    const { onScrolling, onScrollingEnd, updateScroll, ui } = this.props
+    updateScroll(window.scrollY)
+    if (window.scrollY > ui.scrollY) {
+      onScrolling()
+      if (this.scrollingTimeout) {
+        clearTimeout(this.scrollingTimeout)
+      }
+      this.scrollingTimeout = setTimeout(onScrollingEnd, 450)
+    }
+  }
+  render() {
+    const { ui: { isScrolling } } = this.props
+    return (
+      <div className={`dock ${isScrolling ? 'dock--hide' : ''}`}>
+        <a href='mailto:stegmannrd@gmail.com?subject=Hello' className='dock__item'>
+          <span className='dock__item-text'>Mail</span>
+          <i className={generate2xIcon('paper-plane')}></i>
+        </a>
+        <NavLink to='/' className='dock__item'>
+          <span className='dock__item-text'>Home</span>
+          <i className={generate2xIcon('home')}></i>
+        </NavLink>
+        <NavLink to='/blog' className='dock__item'>
+          <span className='dock__item-text'>Blog</span>
+          <i className={generate2xIcon('list')}></i>
+        </NavLink>
+        <NavLink to='/about/languages' className='dock__item'>
+          <span className='dock__item-text'>About</span>
+          <i className={generate2xIcon('portrait')}></i>
+        </NavLink>
+      </div>
+    )
+  }
+}
 
-export default Dock
+const mapStateToProps = ({ ui }) =>
+({
+  ui
+})
+
+const mapDispatchToProps = dispatch =>
+({
+  updateScroll: scrollY => dispatch(updateScroll(scrollY)),
+  onScrolling: () => dispatch(onScrolling()),
+  onScrollingEnd: () => dispatch(onScrollingEnd())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dock)
--- app/src/js/layouts/RootLayout.js
+++ app/src/js/layouts/RootLayout.js
@@ -1,54 +1,13 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-
-import { onScrolling, onScrollingEnd, updateScroll } from 'actions/ui'
+import React from 'react'
 
 import Header from 'components/Header'
 import Dock from 'components/Dock'
 
-class RootLayout extends Component {
-  componentDidMount() {
-    document.addEventListener('scroll', () => this.scrollHandle())
-  }
-  componentWillUnmount() {
-    document.removeEventListener('scroll', () => this.scrollHandle())
-  }
-  scrollHandle() {
-    const { onScrolling, onScrollingEnd, updateScroll, ui } = this.props
-    updateScroll(window.scrollY)
-    if (window.scrollY > ui.scrollY) {
-      onScrolling()
-      if (this.scrollingTimeout) {
-        clearTimeout(this.scrollingTimeout)
-      }
-      this.scrollingTimeout = setTimeout(onScrollingEnd, 450)
-    }
-  }
-  render() {
-    const { children, ui } = this.props
-    return (
-      <div className='root'>
-        <Header />
-        {children}
-        <Dock isScrolling={ui.isScrolling} />
-      </div>
-    )
-  }
-}
+const RootLayout = ({ children }) =>
+  <div className='root'>
+    <Header />
+    {children}
+    <Dock />
+  </div>
 
-const mapStateToProps = ({ ui }) =>
-({
-  ui
-})
-
-const mapDispatchToProps = dispatch =>
-({
-  updateScroll: scrollY => dispatch(updateScroll(scrollY)),
-  onScrolling: () => dispatch(onScrolling()),
-  onScrollingEnd: () => dispatch(onScrollingEnd())
-})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(RootLayout)
+export default RootLayout
