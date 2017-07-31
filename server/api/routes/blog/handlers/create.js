a158e35d03231bd09afe526cacf77c5d9686d9b6
Ryan <stegartie@gmail.com>
2017-07-31 00:15:54 -0700

    Clean up errors with creating blog post

--- server/api/routes/blog/handlers/create.js
+++ server/api/routes/blog/handlers/create.js
@@ -5,26 +5,31 @@ const shortId = require('shortid')
 
 const { allPass, merge, path, pick, pipe, isNil } = require('ramda')
 
-const postAttributes = ['title', 'keywords', 'text', 'image']
+const postAttributes = ['title', 'slug', 'keywords', 'text', 'image']
+
+const getUser = p => path([ 'user', p ])
+const getUserId = getUser('id')
+
+const getPost = p => path([ 'body', 'post', p ])
+const getSlug = getPost('slug')
 
 const validate = req =>
   Post.findOne({
-    where: { slug }
+    where: { slug: getSlug(req) }
   })
   .then(post =>
     post ?
       Promise.reject('slug exists')
-      : slug
+      : req.body.post
   )
 
 module.exports = (req, res) =>
   validate(req)
-    .then(slug => {
+    .then(post => {
       const newPost = merge({
-        userId: req.user.id,
-        slug
-      }, pick(postAttributes, req.body.post))
+        userId: getUserId(req)
+      }, pick(postAttributes, post))
       return Post.create(newPost, { plain: true })
     })
     .then(post => res.status(200).json({post}))
-    .catch(error => res.status(400).json({error})) //TODO: return custom error handling
+    .catch(error => res.status(400).json({error}))
