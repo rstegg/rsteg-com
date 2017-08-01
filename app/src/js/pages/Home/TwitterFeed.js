843a5f437f5b0d81d0a5e6b86facf00c32769a09
Ryan <stegartie@gmail.com>
2017-07-31 18:35:40 -0700

    Update twitter feed sizing

--- app/src/js/pages/Home/TwitterFeed.js
+++ app/src/js/pages/Home/TwitterFeed.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-const height = screen.height <= 1024 ? screen.height * 0.5 : '275'
+const height =
+  screen.height
+
+const width =
+  screen.width
+
+const getHeight = () => {
+  if (width <= 1024) {
+    if (height < 768) {
+      return height * 0.5
+    }
+    return height * 0.75
+  }
+  return '345'
+}
 
 const TwitterFeed = () =>
   <div className='home__twitter-feed'>
-    <a className='twitter-timeline' data-height={height} href='https://twitter.com/rstegd'>
+    <a className='twitter-timeline' data-height={getHeight()} href='https://twitter.com/rstegd'>
       Tweets by rstegd
     </a>
     <script async src='//platform.twitter.com/widgets.js' charSet='utf-8'></script>
